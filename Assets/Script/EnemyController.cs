7272e0ce48f72142e307022f0175579cccf15a9e
Plackpegasus <nicos4001@gmail.com>
2023-11-10 21:58:05 +0100

    add prototype enemies and prefabs

--- Assets/Script/EnemyController.cs
+++ Assets/Script/EnemyController.cs
@@ -7,12 +7,14 @@ public class EnemyController : MonoBehaviour
 
     public float speed = 1f;
 
+    // testing variables
     private bool up = true;
+    private Vector3 startingPos;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        startingPos = transform.position;
     }
 
     // Update is called once per frame
@@ -20,13 +22,21 @@ public class EnemyController : MonoBehaviour
     {
         if (up)
         {
-            transform.position += Vector3.up * Time.deltaTime * speed;
+            transform.position += Vector3.forward * Time.deltaTime * speed;
         } 
         else
         {
-            transform.position -= Vector3.up * Time.deltaTime * speed;
+            transform.position += Vector3.back * Time.deltaTime * speed;
         }
 
-        if (transform.position.y >= 5f || transform.position.y <= 1f) up = !up;
+        if (transform.position.z >= startingPos.z + 5f || transform.position.z <= startingPos.z - 1f) up = !up;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Bullet")
+        {
+            Destroy(gameObject);
+        }
     }
 }
