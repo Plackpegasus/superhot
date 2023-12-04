19b91fd10a97ba191f910a688b3a15a0c9a4cc05
Elia Rohrbach <125868242+eliarohrbach@users.noreply.github.com>
2023-12-04 11:33:16 +0100

    rotating enemies to main camera + disabled movement

--- Assets/Script/EnemyController.cs
+++ Assets/Script/EnemyController.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    // TODO implementation
-
     public float speed = 1f;
 
     private bool up = true;
@@ -20,16 +18,25 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Rotate enemies to main camera
+        Vector3 directionToCamera = Camera.main.transform.position - transform.position;
+        directionToCamera.y = 0; // Ensure the enemy only rotates around the y-axis
+        Quaternion lookRotation = Quaternion.LookRotation(directionToCamera);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * speed);
+
+        // The movement code is commented out
+        /*
         if (up)
         {
             transform.position += Vector3.forward * Time.deltaTime * speed;
-        } 
+        }
         else
         {
             transform.position += Vector3.back * Time.deltaTime * speed;
         }
 
         if (transform.position.z >= startingPos.z + 5f || transform.position.z <= startingPos.z - 1f) up = !up;
+        */
     }
 
     private void OnTriggerEnter(Collider other)
