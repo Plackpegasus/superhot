67f2825643ed78349e7e58c96ba95e3e28cd3122
Plackpegasus <nicos4001@gmail.com>
2023-12-20 21:25:53 +0100

    fix crosshair rotation

--- Assets/Script/BulletSpawnerPlayer.cs
+++ Assets/Script/BulletSpawnerPlayer.cs
@@ -5,14 +5,13 @@ public class BulletSpawnerPlayer : BulletSpawner
 {
     public Image crosshair;
     public AudioSource gunSound;
-    private float targetCrosshairRotation;
+    private float targetCrosshairRotation = -90f; // turn crosshair clockwise
     private float angleVelocity;
 
     // Start is called before the first frame update
     void Start()
     {
-        angleVelocity = -90f / GunCooldown;
-        targetCrosshairRotation = crosshair.transform.rotation.eulerAngles.z;
+        angleVelocity = targetCrosshairRotation / GunCooldown;
     }
 
     // Update is called once per frame
@@ -25,27 +24,13 @@ public class BulletSpawnerPlayer : BulletSpawner
             Vector3 bulletSpawnPosition = bulletSpawnPoint.position;
             bulletSpawnPosition += bulletSpawnPoint.forward; // offset bullet from camera, so playermodel is not hit
             spawnBullet(bulletSpawnPosition, bulletSpawnPoint.rotation);
-            targetCrosshairRotation = crosshair.transform.rotation.eulerAngles.z + -90f; // turn crosshair clockwise
             gunSound.Play();
         }
 
-        Debug.Log("z: " + crosshair.transform.rotation.eulerAngles.z + " target: " + targetCrosshairRotation);
-
-        if (crosshair.transform.rotation.eulerAngles.z >= targetCrosshairRotation)
-        //if (Time.time < passedCooldownTime)
+        if (Time.time < passedCooldownTime)
         {
             float newCrosshairRotation = crosshair.transform.rotation.eulerAngles.z + (angleVelocity * Time.deltaTime);
             crosshair.transform.rotation = Quaternion.Euler(0, 0, newCrosshairRotation);
-            //if (crosshair.transform.rotation.z <= targetCrosshairRotation)
-            //{
-            //    Debug.Log("cooldown");
-
-            //    GameObject newCrosshair = crosshair.gameObject;
-            //    //newCrosshair.transform.rotation.eulerAngles = Vector3.zero;
-
-            //    Destroy(crosshair.gameObject);
-            //    crosshair = newCrosshair.GetComponent<Image>();
-            //}
-        }
+        } 
     }
 }
